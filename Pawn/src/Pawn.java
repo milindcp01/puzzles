282be6bfba7a0cb8f3361489b25e2976f2406bf7
trod <boris_w@mail.ru>
2014-02-13 02:12:34 +0400

    solve Euler problem for NxN case.

---
+++ Pawn/src/Pawn.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Pawn {
+	public static int[][] playGround = generateMatrix(50, 50);
+
+	public static int[][] generateMatrix(int m, int n) {
+		int[][] playGround = new int[m][n];
+		Random randInt = new Random();
+		for (int x = 0; x < n; x++) {
+			for (int y = 0; y < m; y++) {
+				playGround[x][y] = randInt.nextInt(4);
+				System.out.print(playGround[x][y] + " ");
+			}
+			System.out.println();
+		}
+		return playGround;
+	}
+
+	public static int solvePawn() {
+		int res = 0;
+
+		for (int i = playGround.length - 2; i >= 0; i--) {
+			for (int j = playGround[i].length - 2; j >= 0; j--) {
+				if (i == j) {
+					
+					playGround[i][j] += Math.min(playGround[i + 1][j + 1]
+							+ playGround[i + 1][j], playGround[i + 1][j + 1]
+							+ playGround[i][j + 1]);					
+				}
+			}
+		}
+		return playGround[0][0];
+
+	}
+
+	public static void main(String[] args) {		
+		System.out.println("done. result = "+solvePawn());
+	}
+}
