56918451ba2b3b56fb19335a527391b0b05fbdf7
trod <boris_w@mail.ru>
2014-02-17 19:01:43 +0400

    initial commit couple of frame calsses

---
+++ KnightPath/src/knightpath/KnightPath.java
@@ -0,0 +1,23 @@
+package testy;
+
+import java.util.ArrayList;
+
+class Board {
+	int n;
+	ArrayList<BoardCell> board;
+}
+
+class BoardCell {
+	boolean isVisited = false;
+	int x, y = 0;
+	int distance = 0;
+	int distanceToEnd = 0;
+}
+
+public class KnightPath {
+
+	public static void main(String args) {
+
+	}
+
+}
