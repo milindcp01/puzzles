9c72b1e4a25bc9d79c5b353d743e91ae91a7ea71
trod <boris_w@mail.ru>
2014-03-18 18:26:04 +0400

    start Token implementation

--- eval/src/Expression.java
+++ eval/src/Expression.java
@@ -1,14 +1,18 @@
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.Stack;
+import java.util.regex.*;
 
 public class Expression {
-	// main here for quick function test proposal. Will be moved to separate test package.
+	// main here for quick function test proposal. Will be moved to separate
+	// test package.
 	public static void main(String[] args) {
 		String exp1 = "))((";
 		System.out.println(isValidExpression(exp1));
 		String exp2 = "()(())";
 		System.out.println(isValidExpression(exp2));
+		String test = "(23+3)*5+(14+2)";
+
+		generateTokens("(23+3)*5+(14+2)");
 	}
 
 	public static boolean isValidExpression(String expString) {
@@ -27,12 +31,14 @@ public class Expression {
 			BigDecimal apply(BigDecimal x, BigDecimal y) {
 				return x.subtract(y);
 			}
-		}, MULTIPLE("*") {
+		},
+		MULTIPLE("*") {
 			@Override
 			BigDecimal apply(BigDecimal x, BigDecimal y) {
 				return x.multiply(y);
 			}
-		}, DIVIDE("/") {
+		},
+		DIVIDE("/") {
 			@Override
 			BigDecimal apply(BigDecimal x, BigDecimal y) {
 				return x.divide(y);
@@ -45,6 +51,15 @@ public class Expression {
 		}
 
 		abstract BigDecimal apply(BigDecimal x, BigDecimal y);
+	}
+
+	/*
+	 * function add brackets around each number
+	 *
+	 * @param expression
+	 */
+	public static String addBrackets(String input) {
+		return input.replaceAll("([0-9]+)", "($1)");
 	}
 
 	/*
@@ -69,6 +84,54 @@ public class Expression {
 			}
 		}
 		return (counter == elements.length && bracketCount != 0) ? false : true;
+	}
+
+	static class Token {
+		public Stack<String> operands = new Stack<String>();
+		public Stack<Operation> operators = new Stack<Operation>();
+		public Stack<Token> tokens = new Stack<Token>();
+		StringBuilder expression = new StringBuilder();
+
+		public void addNestedToken(Token nestedToken) {
+			this.tokens.push(nestedToken);
+
+		}
+
+		@Override
+		public String toString() {
+			return expression.toString();
+
+		}
+	}
+
+
+
+	private static void generateTokens(String inputStr) {
+		Stack<String> operands = new Stack<String>();
+		Stack<Character> operators = new Stack<Character>();
+		Stack<Token> tokens = new Stack<Token>();
+
+		char[] chars = inputStr.toCharArray();
+		boolean openToken = false;
+		Token t = null;
+		for (char c : chars) {
+			if (Character.toString(c).equals("(") && !openToken) {
+				t = new Token();
+				openToken = true;
+			} else if (Character.toString(c).equals("(") && openToken) {
+				// case of nested brackets
+				tokens.firstElement().addNestedToken(t);
+			}
+			if (!Character.toString(c).equals(")")
+					&& !Character.toString(c).equals("(")) {
+				t.expression.append(Character.toString(c));
+			}
+			if (Character.toString(c).equals(")") && openToken) {
+				tokens.push(t);
+				openToken = false;
+				System.out.println("Token: " + t.toString());
+			}
+		}
 	}
 
 }
